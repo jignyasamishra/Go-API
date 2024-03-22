b83c2fcfab28309ee36b21cd059c90dabaa5d4c2
jignyasamishra <iamjignyasa@gmail.com>
2024-03-22 10:00:38 +0530

    add
    Signed-off-by: jignyasamishra <iamjignyasa@gmail.com>

---
+++ internal/middleware/authorization.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/jignnyasamishra/Go-API/api"
+	log "github.com/sirupsen/logrus"
+)
+
+var UnAuthroizedError = errors.New("Invalid username or token.")
+
+func Authorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+
+		if username == "" || token == "" {
+			log.Error(UnAuthroizedError)
+			api.RequestErrorHandler(w, UnAuthroizedError)
+			return
+		}
+		var loginDetails *tools.loginDetails
+		loginDetails = (*database).GetUserLoginDetails(username)
+		if loginDetails == nil || (token != (*loginDetails).AuthToken) {
+			api.RequestErrorHandler(w, UnAuthroizedError)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+
+}
