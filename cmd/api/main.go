0adf6cfe43b48959b10155d5189f338d8782c312
jignyasamishra <iamjignyasa@gmail.com>
2024-03-22 09:34:56 +0530

    api handlers
    Signed-off-by: jignyasamishra <iamjignyasa@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,22 @@
 package main
+
+import(
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi"
+	"github.com/jignyasamishra/Go-API/internal/handlers"
+	log "github.com/sirupsen/logrus"
+	
+)
+
+func main(){
+	log.SetReportCaller(true)
+	ver r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+	fmt.Println("Starting GP API services...")
+
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil{
+		log.Error(err)
+	}
+}
